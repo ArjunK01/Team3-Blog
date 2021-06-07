a042f4a2a138318e1fe0a25ffcb6f8eaeca8f3fa
Connor Noble <noblecp1@gmail.com>
2021-06-07 17:00:24 -0400

    updated user profile ui

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,72 +1,67 @@
-import React, { useState, useEffect } from "react";
-// import "../App.css";
+import React from "react";
+import "./user-profile.css";
 import FaceIcon from "@material-ui/icons/Face";
 
 const UserProfile = () => {
   return (
-    <div className="user-profile-container" style={{ padding: "20px" }}>
-      <div
-        className="top-section"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "left",
-          alignItems: "center",
-          paddingBottom: "20px",
-        }}
-      >
-        <div className="pfp">
-          <FaceIcon
-            style={{ color: "black", fontSize: "15rem", paddingRight: "10px" }}
-          />
+    <div className="container">
+      <div className="top-section">
+        <div className="pfp-container">
+          <FaceIcon style={{ color: "grey", fontSize: "10rem" }} />
+          {/* <div className="handle pfp-container">
+            <p style={{ color: "grey", padding: "0", margin: "0" }}>HANDLE</p>
+            <p>@user12345</p>
+          </div> */}
         </div>
-        <div
-          className="personalInfo"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "left",
-            alignItems: "flex-start",
-            fontSize: "2rem",
-          }}
-        >
-          <div className="name" style={{ flexDirection: "wrap" }}>
-            <p>Name: </p>
-            <p style={{ color: "grey" }}>Example name</p>
+        <div className="personal-info">
+          {/* NAME FIELD */}
+          <div className="name">
+            <p className="reset-padding field-title">NAME&nbsp;</p>
+            <p className="reset-padding">Full Name</p>
           </div>
-          <div className="birthday" style={{ flexDirection: "row" }}>
-            <p>Birthday: </p>
-            <p style={{ color: "grey" }}>1/1/2000</p>
+          {/* HANDLE */}
+          <div className="name">
+            <p className="reset-padding field-title">HANDLE&nbsp;</p>
+            <p className="reset-padding">@user12345</p>
+          </div>
+          {/* BIRTHDAY FIELD */}
+          <div className="birthday">
+            <p className="reset-padding field-title">BIRTHDAY&nbsp;</p>
+            <p className="reset-padding">1/1/2000</p>
+          </div>
+          {/* LOCATION FIELD */}
+          <div className="location">
+            <p className="reset-padding field-title">LOCATION</p>
+            <p className="reset-padding">San Francisco, CA</p>
+          </div>
+          {/* ABOUT FIELD */}
+          <div className="about">
+            <p className="reset-padding field-title">About</p>
+            <p className="reset-padding">I am a sample user</p>
           </div>
         </div>
       </div>
 
-      <div
-        className="bottom-section"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "flex-start",
-        }}
-      >
-        <div className="blog-post-list">
-          <h2>Blog posts</h2>
-          <ul style={{ listStyle: "none" }}>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-          </ul>
+      <div className="bottom-section">
+        <div className="blog-posts-container">
+          <div className="subheader">
+            <h2>Blog posts</h2>
+          </div>
+          <div className="blog-content">
+            <p className="blog-post">example item</p>
+            <p className="blog-post">example item</p>
+            <p className="blog-post">example item</p>
+          </div>
         </div>
-        <div className="forum-post-list">
-          <h2>Forum posts</h2>
-          <ul style={{ listStyle: "none" }}>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-          </ul>
+        <div className="forum-posts-container">
+          <div className="subheader">
+            <h2>Forum posts</h2>
+          </div>
+          <div className="forum-content">
+            <p className="forum-post">example item</p>
+            <p className="forum-post">example item</p>
+            <p className="forum-post">example item</p>
+          </div>
         </div>
       </div>
     </div>
