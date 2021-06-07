159a7d841c3f70c63a7538c4a826f1cbe485eb73
lghoerr <63504311+lghoerr@users.noreply.github.com>
2021-06-07 13:56:55 -0700

    Home page banner image and text added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,22 +4,51 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import "../App.css";
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 600,
+    backgroundColor: "",
+    color: "#446DAB",
+  },
+});
+
 const HomePage = () => {
+  const classes = useStyles();
   const windowWidth = window.innerWidth;
   return (
     <div>
-      <div style={{ float: "center", color: "black" }}>
-        <img
-          src={banner}
-          alt="City Street"
-          style={{
-            position: "absolute",
-            width: windowWidth + 1,
-            left: 0,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        ></img>
+      <Card>
+        <div style={{ float: "center" }}>
+          <img
+            src={banner}
+            alt="City Street"
+            style={{
+              position: "absolute",
+              width: windowWidth + 1,
+              height: window.innerHeight * 0.75,
+              left: 0,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          ></img>
+        </div>
+      </Card>
+      <div
+        style={{
+          textAlign: "left",
+          marginRight: 20,
+          marginLeft: 20,
+          marginBottom: 50,
+          top: window.innerHeight - 200,
+          position: "relative",
+          fontSize: 30,
+        }}
+      >
+        <Card className={classes.root}>
+          <CardContent>
+            <p>Welcome to City Girl Summer!</p>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
