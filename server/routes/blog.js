b1ab89226054ea9762b753cd2f8230a26fded23c
akd2yf <akd2yf@virginia.edu>
2021-06-08 11:16:49 -0400

    initial backend for blog collection in database

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -6,4 +6,169 @@ router.get("/", (req, res) => {
   res.send("test");
 });
 
+/**
+ * Retrieve all blogs from collection
+ * Sends as an array
+ */
+router.get("/get", (req, res) => {
+  console.log('here')
+  const blogRef = db.collection("blogs");
+  console.log('here')
+  let temp = [];
+  blogRef
+    .get()
+    .then((resp) => {
+      resp.forEach((doc) => {
+        temp.push(doc.data());
+      });
+    })
+    .then(() => {
+      res.send(temp);
+    });
+});
+
+/**
+ * Create a new blog post
+ * Request body includes all blog information
+ */
+router.post("/create", async(req, res) => {
+  await db.collection("blogs").doc(req.body.id).set(req.body);
+
+  res.sendStatus(200);
+});
+
+/**
+ * Adds a like to the blog post
+ * Increments the likes field, and updates the blog post
+ * Sends status 200 if successful, 404 otherwise
+ */
+router.put("/like", (req, res) => {
+  const { blog_id } = req.body;
+  db.collection("blogs").doc(blog_id).get()
+  .then((doc) => {
+    if (doc.exists) {
+      curr_likes = doc.data().likes;
+    }
+    else {
+      res.sendStatus(404);
+    }
+    curr_likes = curr_likes+1;
+
+    db.collection("blogs").doc(blog_id)
+    .update({
+      likes: curr_likes,
+    })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      res.sendStatus(404);
+    });
+  });
+});
+/**
+ * Deletes the blog with id given in body
+ * returns 200 if done successfully
+ */
+router.delete("/delete", async(req, res) => {
+  const { id } = req.body;
+
+  await db.collection("events").doc(id).delete();
+
+  res.sendStatus(200);
+});
+/**
+ * Edits the blog
+ * All new blog info in body
+ * Sends 200 when successful
+ */
+router.put("/edit", async(req, res) => {
+  await db.collection("blogs").doc(req.body.id).set(req.body);
+
+  res.sendStatus(200);
+});
+/**
+ * Retrieve all comments from the blog post
+ * @param id - the ID of the blog post whose comments you're retrieving
+ * Sends an array of comments
+ */
+router.get("/comments/get/:id", (req, res) => {
+  const commentRef = db.collection("blogs").doc(req.params.id).collection("comments");
+  let temp = [];
+  commentRef
+    .get()
+    .then((resp) => {
+      resp.forEach((doc) => {
+        temp.push(doc.data());
+      });
+    })
+    .then(() => {
+      res.send(temp);
+    });
+});
+/**
+ * Posts new comment the blog post
+ * Body is all the information for the new comment
+ * @param id - the ID of the blog post being commented on
+ * Sends 200 if comment made succesffully
+ */
+router.post("/comments/create/:id", async(req, res) => {
+  await db.collection("blogs").doc(req.params.id).collection("comments").push(req.body);
+
+  res.sendStatus(200);
+});
+/**
+ * Likes comment
+ * Body is the comment id
+ * @param id - the ID of the blog post being commented on
+ * Sends 200 if like made succesffully
+ */
+router.put("/comments/like/:id", (req, res) => {  
+  const { comment_id } = req.body;
+  db.collection("blogs").doc(req.params.id).collection("comments").get()
+  .then((doc) => {
+    if (doc.exists) {
+      curr_likes = doc.data().likes;
+    }
+    else {
+      res.sendStatus(404);
+    }
+    curr_likes = curr_likes+1;
+
+    db.collection("blogs").doc(blog_id)
+    .update({
+      likes: curr_likes,
+    })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      res.sendStatus(404);
+    });
+  });
+  res.sendStatus(200);
+});
+/**
+ * Deletes a comment
+ * ID of the comment in body
+ * @param id - blog post whose comment is being deleted
+ */
+router.delete("/comments/delete/id:", async(req, res) => {
+  const { id } = req.body;
+
+  await db.collection("events").doc(req.params.id).collection("comments").doc(id).delete();
+
+  res.sendStatus(200);
+});
+/**
+ * Edits a comment
+ * All new comment information in body
+ * @param id - the blog post whose comment is being edited
+ */
+router.put("/comments/edit/id:", async(req, res) => {
+  await db.collection("blogs").doc(req.params.id).collection("comments").doc(req.body.id).set(req.body);
+
+  res.sendStatus(200);
+});
+
 module.exports = router;
