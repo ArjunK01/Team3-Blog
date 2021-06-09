c1acba5fe2a037c9890e1faa97e9968d5c68c3e5
Khushi Chawla <emailkhushic@gmail.com>
2021-06-09 09:58:47 -0400

    fix get request to get all info about a user

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,18 +5,53 @@ const db = require("../firebase");
 //get user by id
 router.get("/get/:id", async (req, res) => {
   var user_id = req.params.id;
-  db.collection("user").doc(user_id).get()
-  .then((doc) => {
-    if (doc.exists) {
-      res.send({ id: doc.id, ...doc.data()});
-    }
-    else {
-      res.sendStatus(404);
-    }
-  })
-  .catch((error) => {
+  var doc = await db.collection("user").doc(user_id).get();
+  //get all data about that user
+  if (doc.exists) {
+    var user_data = { id: doc.id, ...doc.data()};
+  }
+  else{
     res.sendStatus(404);
-  });
+  }
+
+  //get merch data
+  for(let i = 0; i < user_data.purchasedMerch.length; i++){
+    let merch_data = await db.collection("merch").doc(user_data.purchasedMerch[i]).get()
+    user_data.purchasedMerch[i] = { id: merch_data.id, ...merch_data.data()};
+  }
+
+  //get liked blog posts
+  for(let i = 0; i < user_data.likedBlogPosts.length; i++){
+    let bloglike_data = await db.collection("blogs").doc(user_data.likedBlogPosts[i]).get()
+    user_data.likedBlogPosts[i] = { id: bloglike_data.id, ...bloglike_data.data()};
+  }
+  //get blog post comments
+  for(let i = 0; i < user_data.blogComments.length; i++){
+    let blog_id = user_data.blogComments[i].blog_id;
+    let comment_id = user_data.blogComments[i].comment_id;
+    let blogcomment_data = await db.collection("blogs").doc(blog_id).collection("comments").doc(comment_id).get();
+    user_data.blogComments[i] = { id: blogcomment_data.id, ...blogcomment_data.data()};
+  }
+
+  //get forum posts
+  for(let i = 0; i < user_data.forumPosts.length; i++){
+    let forumpost_data = await db.collection("forums").doc(user_data.forumPosts[i]).get()
+    user_data.forumPosts[i] = { id: forumpost_data.id, ...forumpost_data.data()};
+  }
+  //get liked forum posts
+  for(let i = 0; i < user_data.forumLikes.length; i++){
+    let forumlike_data = await db.collection("forums").doc(user_data.forumLikes[i]).get()
+    user_data.forumLikes[i] = { id: forumlike_data.id, ...forumlike_data.data()};
+  }
+  //get forum post comments
+  for(let i = 0; i < user_data.forumComments.length; i++){
+    let forum_id = user_data.forumComments[i].forum_id;
+    let comment_id = user_data.forumComments[i].comment_id;
+    let forumcomment_data = await db.collection("forums").doc(forum_id).collection("comments").doc(comment_id).get();
+    user_data.forumComments[i] = { id: forumcomment_data.id, ...forumcomment_data.data()};
+  }
+
+  res.send(user_data);
 })
 
 //delete user
