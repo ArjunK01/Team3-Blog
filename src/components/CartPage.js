f5a3f08bb5427d5e0aeb6597762901bb9900473b
lghoerr <63504311+lghoerr@users.noreply.github.com>
2021-06-11 05:35:34 -0700

    cart

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,20 +1,41 @@
-import React, { useContext } from "react";
 import { CartContext } from "../context/CartProvider";
 import image from "../images/shirt.jpeg";
+import React, { useState, useEffect, useContext } from "react";
+import "../styles/cart.css";
+import Product from "./Product";
 
 const CartPage = () => {
   const { cart } = useContext(CartContext);
+
   return (
     <div>
-      {cart &&
-        cart.map(item => {
-          return (
-            <p>
-              {item.title} - {item.price} - {item.stock} - {item.description}
-              <img style={{ height: "100px", width: "100px" }} src={image} />
-            </p>
-          );
-        })}
+      <div className="cart-page-title">Cart</div>
+      <div className="cart-container">
+        <div className="cart-header"></div>
+        {/* Number of products in the user's cart */}
+        <h4 className="product-count">
+          You have {cart.length} product(s) in cart.
+        </h4>
+        <div className="products-container">
+          {cart &&
+            cart.map((item) => {
+              return (
+                <p>
+                  <img
+                    style={{ height: "100px", width: "100px" }}
+                    src={image}
+                  />
+                  <Product
+                    title={item.title}
+                    price={item.price}
+                    description={item.description}
+                    src={image}
+                  />
+                </p>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 };
